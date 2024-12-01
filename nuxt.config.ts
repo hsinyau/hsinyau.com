0a75b4139c8899f55159b15cbe68d6ffb5db203e
hsinyau <2737164694@qq.com>
2024-12-01 23:05:14 +0800

    chore: @nuxt/ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,20 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+  modules: ['@nuxt/ui'],
+  // Nuxt UI Module Config
+  ui: {
+    safelistColors: [
+      'gray',
+      'zinc',
+      'red',
+      'orange',
+      'amber',
+      'green',
+      'emerald',
+      'sky',
+      'blue',
+      'purple',
+    ],
+  },
 })
