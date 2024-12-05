90898d6685382a4d4082e2af76b04c8f5b772d48
hsinyau <2737164694@qq.com>
2024-12-06 04:57:20 +0800

    fix: You should use slots with <ContentQuery>!

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,9 @@
+<script setup lang="ts">
+const { data: home } = await useAsyncData('home', () => queryContent('/').findOne())
+</script>
+
 <template>
   <main class="!max-w-none prose dark:prose-invert">
-    <ContentDoc path="/" />
+    <ContentDoc :path="home?.path" />
   </main>
 </template>
