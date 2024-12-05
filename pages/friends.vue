68832fdec648a772dc364a60510e788d47d62451
hsinyau <2737164694@qq.com>
2024-12-05 23:30:25 +0800

    chore: 友情链接页面优化

--- pages/friends.vue
+++ pages/friends.vue
@@ -16,33 +16,31 @@ const { data: friends } = await useAsyncData('all-friends', () => queryContent('
       :title
       :description
     />
-    <ul class="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-4 place-content-between">
-      <template v-for="friend in friends" :key="friend._id">
-        <li v-for="item in friend.body" :key="item.link">
-          <NuxtLink
-            :href="item.link"
-            target="_black"
-            class="flex items-center gap-4 cursor-pointer relative group"
-          >
-            <div class="group-hover:rotate-[360deg] transition-transform duration-500">
-              <UAvatar
-                :src="item.avatar"
-                alt="Avatar"
-                size="2xl"
-                placeholder
-              />
-            </div>
-            <div class="space-y-3">
-              <h3 class="text-base font-medium line-clamp-1">
-                {{ item.name }}
-              </h3>
-              <p class="text-sm text-gray-500 line-clamp-1">
-                {{ item.desc }}
-              </p>
-            </div>
-          </NuxtLink>
-        </li>
-      </template>
+    <ul class="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-6 place-content-between">
+      <li v-for="item in friends[0].body" :key="item.link">
+        <NuxtLink
+          :href="item.link"
+          target="_black"
+          class="flex items-center gap-4 cursor-pointer relative group"
+        >
+          <div class="group-hover:rotate-[360deg] transition-transform duration-500">
+            <UAvatar
+              :src="item.avatar"
+              alt="Avatar"
+              size="2xl"
+              placeholder
+            />
+          </div>
+          <div class="space-y-3">
+            <h3 class="text-base font-medium line-clamp-1">
+              {{ item.name }}
+            </h3>
+            <p class="text-sm text-gray-500 line-clamp-1">
+              {{ item.desc }}
+            </p>
+          </div>
+        </NuxtLink>
+      </li>
     </ul>
   </main>
 </template>
