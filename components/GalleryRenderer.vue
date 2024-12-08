00955789ee70832396eee902084d53b2a72677dd
hsinyau <2737164694@qq.com>
2024-12-08 20:00:06 +0800

    fix: ipx 提供者未定义

--- components/GalleryRenderer.vue
+++ components/GalleryRenderer.vue
@@ -6,18 +6,29 @@ const props = defineProps<PhotoRendererMetadata>()
 const nuxtImgProps = computed(() => {
   const { src, width, height, sizes: imgSize, loading, decoding, title, alt } = props.imageProps
   const sizes = 'xs sm md lg xl xxl 2xl'.split(' ').map(screen => `${screen}:${imgSize}`).join(' ')
-  const provider = src?.includes('https://') ? 'ipx' : 'cloudflare'
-  return { src, width, height, sizes, loading, decoding, title, alt, provider }
+  return { src, width, height, sizes, loading, decoding, title, alt }
 })
+
+const isCloudflare = computed(() => !nuxtImgProps.value.src?.includes('https://'))
 </script>
 
 <template>
   <div class="overflow-hidden cursor-pointer">
     <NuxtImg
+      v-if="isCloudflare"
+      provider="cloudflare"
       v-bind="nuxtImgProps"
       loading="lazy"
       placeholder
       class="rounded-md hover:scale-105 transition-all duration-500 dark:brightness-75"
     />
+    <img
+      v-else
+      v-bind="nuxtImgProps"
+      loading="lazy"
+      placeholder="blur"
+      alt="not from cloudflare"
+      class="rounded-md hover:scale-105 transition-all duration-500 dark:brightness-75"
+    >
   </div>
 </template>
