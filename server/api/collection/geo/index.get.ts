29f0551b12a8c52cc2a15600651dcfc7308075a0
hsinyau <2737164694@qq.com>
2024-12-16 09:38:37 +0800

    chore: api endpoint

--- server/api/collection/geo/index.get.ts
+++ server/api/collection/geo/index.get.ts
@@ -1,14 +1,5 @@
 export default defineCachedEventHandler(async () => {
-  const data = await $fetch('https://cdn.jsdelivr.net/gh/hsinyau/static@master/data/geo.json')
+  const data = await $fetch('https://static.758201.xyz/data/geo.json')
 
   return data
 })
-
-// export default defineCachedEventHandler(async () => {
-//   const data = await $fetch('https://cdn.jsdelivr.net/gh/hsinyau/static@master/data/geo.json')
-
-//   return data
-// }, {
-//   maxAge: 24 * 60 * 60 * 30,
-//   name: 'geo',
-// })
