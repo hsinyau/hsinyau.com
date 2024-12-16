225b6ba2c9ea2f636e07e8cf6ed943fedd4f4a4f
hsinyau <2737164694@qq.com>
2024-12-16 09:34:37 +0800

    chore: 移除lanyard

--- server/api/activity/index.get.ts
+++
@@ -1,5 +0,0 @@
-export default defineEventHandler(async (event) => {
-  const { discord } = useRuntimeConfig(event)
-  const data = await $fetch(`https://api.lanyard.rest/v1/users/${discord.userId}`)
-  return data
-})
