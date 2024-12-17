8f2e10c51739e648091dbc8a1e1b97def1e8a76e
hsinyau <2737164694@qq.com>
2024-12-18 03:23:27 +0800

    chore: bilibili 嵌入组件

---
+++ components/content/Bilibili.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+defineProps({
+  src: {
+    type: String,
+    require: true,
+  },
+})
+</script>
+
+<template>
+  <div class="flex justify-center my-4">
+    <div class="relative h-0 w-full" style="padding-bottom: 50%;">
+      <iframe
+        :src="src"
+        scrolling="no"
+        frameborder="no"
+        auto
+        class="absolute inset-0 size-full rounded-md border-0"
+      />
+    </div>
+  </div>
+</template>
