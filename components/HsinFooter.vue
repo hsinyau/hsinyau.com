382e44502a799b0f42f3c8c42f7817b3a2540218
hsinyau <2737164694@qq.com>
2024-12-18 19:20:32 +0800

    chore: 页脚添加 Moe ICP

--- components/HsinFooter.vue
+++ components/HsinFooter.vue
@@ -37,8 +37,17 @@ const { socials } = useAppConfig()
         </div>
       </div>
     </div>
-    <div class="mt-8 w-full flex justify-center text-xs">
-      © {{ new Date().getFullYear() }} Hsinyau. All rights reserved.
+    <div class="text-xs text-gray-500 pt-6 text-center">
+      <span>
+        &copy;
+        2022 -
+        {{ new Date().getFullYear() }}
+        <NuxtLink href="/" class="hover:underline">
+          𝐻𝑠𝑖𝑛𝑦𝑎𝑢
+        </NuxtLink>
+        <span class="px-1">•</span>
+        <NuxtLink href="https://icp.gov.moe/?keyword=20242023" target="_blank" class="hover:underline">萌ICP备20242023号</NuxtLink>
+      </span>
     </div>
   </footer>
 </template>
