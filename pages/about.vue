60b9b64dea5135bfbc7f30508fcf3d4ab2a02ca0
hsinyau <2737164694@qq.com>
2025-01-18 19:25:53 +0800

    style: Avatar 样式微调

--- pages/about.vue
+++ pages/about.vue
@@ -16,15 +16,14 @@ useSeoMeta({
 <template>
   <main class="!max-w-none prose dark:prose-invert">
     <div class="relative flex justify-center filter dark:brightness-50 pb-8">
-      <div class="relative w-[134px] h-[134px] bg-gray-200 rounded-full overflow-hidden flex items-center justify-center">
-        <span class="absolute inset-5 rounded-full bg-white z-1" />
+      <div class="flex items-center justify-center">
         <NuxtImg
           provider="cloudflare"
           src="/image/avatar.jpg"
           alt="Avatar"
           width="128"
           height="128"
-          class="m-auto rounded-full overflow-hidden w-32 h-32 z-10"
+          class="m-auto rounded-full overflow-hidden w-32 h-32 border-2 border-zinc-200 z-10"
           placeholder
         />
       </div>
