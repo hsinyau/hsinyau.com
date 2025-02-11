a56023c9c0ffb2338597bdfd792943d5d0c7f9ea
hsinyau <2737164694@qq.com>
2025-02-12 02:59:08 +0800

    chore: 添加 MBTI 详细信息

---
+++ components/content/Mbti.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="my-10 grid grid-cols-1 md:grid-cols-2 gap-10 not-prose">
+    <a
+      href="https://www.16personalities.com/ch/infj-%E4%BA%BA%E6%A0%BC"
+      rel="noopener noreferrer"
+      target="_blank"
+      class="flex flex-col sm:flex-row items-center justify-center gap-4"
+    >
+      <NuxtImg
+        provider="cloudflare"
+        src="/image/INFJ-T.svg"
+        alt="INFJ-T"
+        width="160"
+        height="160"
+        class="w-40 h-40"
+        placeholder
+      />
+      <div>
+        <b><span class="uppercase text-xl text-[#99C26C]">提倡者</span> INFJ-T </b>
+        <p
+          class="mt-4 tracking-wide text-xs text-gray-500"
+        >
+          倡导者可能是最稀有的人格类型，但他们肯定会在世界上留下自己的印记。他们理想主义、有原则，不满足于平平安安地度过一生--他们想要站起来，有所作为。对于倡导者来说，成功不是来自金钱或地位，而是来自寻求成就感，帮助他人，成为世界上一股向善的力量。
+        </p>
+      </div>
+    </a>
+    <div class="min-h-40 flex justify-evenly">
+      <div class="h-full flex flex-col items-center" style="color: rgb(66, 152, 180);">
+        <span class="text-xs">92%</span>
+        <div class="relative my-2 h-full w-3 overflow-hidden rounded-xl bg-gray-200 dark:bg-gray-600">
+          <div class="absolute w-3 rounded-xl" style="background: rgb(66, 152, 180); top: 8%; height: 92%;" />
+        </div><span class="text-xs">内向</span>
+      </div>
+      <div class="h-full flex flex-col items-center" style="color: rgb(228, 174, 58);">
+        <span class="text-xs">66%</span>
+        <div class="relative my-2 h-full w-3 overflow-hidden rounded-xl bg-gray-200 dark:bg-gray-600">
+          <div class="absolute w-3 rounded-xl" style="background: rgb(228, 174, 58); top: 34%; height: 66%;" />
+        </div><span class="text-xs">有远见</span>
+      </div>
+      <div class="h-full flex flex-col items-center" style="color: rgb(51, 164, 116);">
+        <span class="text-xs">75%</span>
+        <div class="relative my-2 h-full w-3 overflow-hidden rounded-xl bg-gray-200 dark:bg-gray-600">
+          <div class="absolute w-3 rounded-xl" style="background: rgb(51, 164, 116); top: 25%; height: 75%;" />
+        </div><span class="text-xs">感受</span>
+      </div>
+      <div class="h-full flex flex-col items-center" style="color: rgb(136, 97, 154);">
+        <span class="text-xs">90%</span>
+        <div class="relative my-2 h-full w-3 overflow-hidden rounded-xl bg-gray-200 dark:bg-gray-600">
+          <div class="absolute w-3 rounded-xl" style="background: rgb(136, 97, 154); top: 10%; height: 90%;" />
+        </div><span class="text-xs">批判</span>
+      </div>
+      <div class="h-full flex flex-col items-center" style="color: rgb(242, 94, 98);">
+        <span class="text-xs">79%</span>
+        <div class="relative my-2 h-full w-3 overflow-hidden rounded-xl bg-gray-200 dark:bg-gray-600">
+          <div class="absolute w-3 rounded-xl" style="background: rgb(242, 94, 98); top: 21%; height: 79%;" />
+        </div><span class="text-xs">起伏不定</span>
+      </div>
+    </div>
+  </div>
+</template>
