1b5ebabfd8f760c3e3a680a9e6d9b45bb13708d4
hsinyau <2737164694@qq.com>
2025-03-08 19:40:52 +0800

    feat: 优化网站滚动条样式和加载指示器

--- app.vue
+++ app.vue
@@ -24,6 +24,7 @@ useHead({
 
 <template>
   <UApp>
+    <NuxtLoadingIndicator color="#808080" />
     <UContainer class="z-50 relative max-w-6xl flex flex-col min-h-[calc(100vh-24px)]">
       <HsinHeader />
       <main class="mt-12 grow">
@@ -34,3 +35,37 @@ useHead({
     </UContainer>
   </UApp>
 </template>
+
+<style>
+* {
+  -webkit-overflow-scrolling: touch;
+  -ms-overflow-style: -ms-autohiding-scrollbar;
+  scrollbar-width: 6px;
+  scrollbar-color: transparent;
+}
+
+::-webkit-scrollbar {
+  height: 6px;
+  width: 6px;
+}
+
+::-webkit-scrollbar-track {
+  background: transparent;
+}
+
+::-webkit-scrollbar-thumb {
+  background: rgba(128, 128, 128, 0.5);
+  border-radius: 3px;
+}
+
+::-webkit-scrollbar-thumb:hover {
+  background: rgba(128, 128, 128, 0.7);
+  opacity: 1;
+  display: block !important
+}
+
+::-webkit-scrollbar-thumb:active {
+  background: rgba(128, 128, 128, 0.9);
+  border-radius: 3px;
+}
+</style>
--- components/BackTop.vue
+++ components/BackTop.vue
@@ -13,29 +13,15 @@ const isShow = computed(() => y.value > 200)
 </script>
 
 <template>
-  <Transition name="fade">
-    <div v-if="isShow">
-      <UTooltip text="回到顶部" :delay-duration="0" @click="scrollToTop">
-        <UButton
-          color="neutral"
-          icon="i-ph-arrow-fat-lines-up-duotone"
-          size="md"
-          variant="outline"
-          class="fixed bottom-8 right-8 focus:outline-none outline-none duration-300 z-[999] cursor-pointer"
-        />
-      </UTooltip>
-    </div>
-  </Transition>
+  <div v-if="isShow">
+    <UTooltip text="回到顶部" :delay-duration="0" @click="scrollToTop">
+      <UButton
+        color="neutral"
+        icon="i-ph-arrow-fat-lines-up-duotone"
+        size="md"
+        variant="outline"
+        class="fixed bottom-8 right-8 focus:outline-none outline-none duration-300 z-[999] cursor-pointer"
+      />
+    </UTooltip>
+  </div>
 </template>
-
-<style scoped>
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.3s ease;
-}
-
-.fade-enter-from,
-.fade-leave-to {
-  opacity: 0;
-}
-</style>
